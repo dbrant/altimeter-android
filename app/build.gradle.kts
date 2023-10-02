2617ec0007e894f3cfc355647eb92f98466dc322
Dmitry Brant <me@dmitrybrant.com>
2023-10-02 12:54:14 -0400

    Clean up a bit.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,6 +17,10 @@ android {
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
+    buildFeatures {
+        viewBinding = true
+    }
+
     buildTypes {
         release {
             isMinifyEnabled = false
@@ -26,10 +30,12 @@ android {
             )
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
+
     kotlinOptions {
         jvmTarget = "17"
     }
@@ -41,6 +47,7 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/com/dmitrybrant/altimeter/MainActivity.kt
+++ app/src/main/java/com/dmitrybrant/altimeter/MainActivity.kt
@@ -3,7 +3,6 @@ package com.dmitrybrant.altimeter
 import android.Manifest
 import android.annotation.SuppressLint
 import android.app.Service
-import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.location.Location
@@ -12,30 +11,30 @@ import android.location.LocationManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.IBinder
-import android.util.Log
-import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.app.ActivityCompat
+import com.dmitrybrant.altimeter.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity(), LocationListener {
+    private lateinit var binding: ActivityMainBinding
 
     private var locationTracker: LocationTracker? = null
 
     private val locationPermissionRequest = registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { permissions ->
         when {
-            permissions[Manifest.permission.ACCESS_FINE_LOCATION] == true ||
-                    permissions[Manifest.permission.ACCESS_COARSE_LOCATION] == true -> {
+            permissions[Manifest.permission.ACCESS_FINE_LOCATION] == true -> {
                 startLocationTracking()
             }else -> {
-                Toast.makeText(this, "Permissions.", Toast.LENGTH_LONG).show()
+                Toast.makeText(this, R.string.permissions_invalid, Toast.LENGTH_LONG).show()
             }
         }
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         if (haveLocationPermissions()) {
             startLocationTracking()
@@ -55,15 +54,18 @@ class MainActivity : AppCompatActivity(), LocationListener {
     }
 
     private fun startLocationTracking() {
-        locationTracker = LocationTracker(this)
+        try {
+            locationTracker?.cleanup()
+            locationTracker = LocationTracker(this)
+        } catch (e: Exception) {
+            Toast.makeText(this, e.localizedMessage, Toast.LENGTH_SHORT).show()
+        }
     }
 
     override fun onLocationChanged(location: Location) {
-        //Log.d("MainActivity", "onLocationChanged: $location")
-
-        findViewById<TextView>(R.id.txtLatitude).text = "Latitude: ${location.latitude}"
-        findViewById<TextView>(R.id.txtLongitude).text = "Longitude: ${location.longitude}"
-        findViewById<TextView>(R.id.txtAltitude).text = "Altitude: ${location.altitude.toInt()} m (${(location.altitude * 3.28084).toInt()} ft)"
+        binding.txtLatitude.text = location.latitude.toString()
+        binding.txtLongitude.text = location.longitude.toString()
+        binding.txtAltitude.text = location.altitude.toInt().toString() //"Altitude: ${location.altitude.toInt()} m (${(location.altitude * 3.28084).toInt()} ft)"
 
     }
 
@@ -72,34 +74,25 @@ class MainActivity : AppCompatActivity(), LocationListener {
         private val activity: MainActivity,
     ) : Service() {
 
-        private val MIN_DISTANCE_CHANGE_FOR_UPDATES = 10f
-        private val MIN_TIME_BW_UPDATES = 1000L * 60 * 1
         private var locationManager: LocationManager = activity.getSystemService(LOCATION_SERVICE) as LocationManager
 
         init {
-
             val gpsEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
-            val networkEnabled = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+            // val networkEnabled = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
 
-            if (!gpsEnabled && !networkEnabled) {
-                Toast.makeText(activity, "Location provider not available.", Toast.LENGTH_SHORT).show()
+            if (!gpsEnabled) {
+                Toast.makeText(activity, R.string.gps_unavailable, Toast.LENGTH_SHORT).show()
             } else {
-                locationManager.requestLocationUpdates(
-                    LocationManager.GPS_PROVIDER,
-                    MIN_TIME_BW_UPDATES,
-                    MIN_DISTANCE_CHANGE_FOR_UPDATES, activity)
-
-                //loc = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-                //if (loc != null) {
-                //    latitude = loc.getLatitude();
-                //    longitude = loc.getLongitude();
-                //}
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 1f, activity)
             }
-
         }
 
         fun cleanup() {
-            locationManager.removeUpdates(activity)
+            try {
+                locationManager.removeUpdates(activity)
+            } catch (e: Exception) {
+                // ignore
+            }
         }
 
         override fun onBind(intent: Intent?): IBinder? {
