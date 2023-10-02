641a51aac6a446355df29cdf13742c8a2700af80
Dmitry Brant <me@dmitrybrant.com>
2023-10-02 17:05:50 -0400

    Add options to switch units.

--- app/src/main/java/com/dmitrybrant/altimeter/MainActivity.kt
+++ app/src/main/java/com/dmitrybrant/altimeter/MainActivity.kt
@@ -5,6 +5,7 @@ import android.annotation.SuppressLint
 import android.app.Service
 import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import android.content.pm.PackageManager
 import android.hardware.GeomagneticField
 import android.hardware.Sensor
@@ -16,14 +17,20 @@ import android.location.LocationListener
 import android.location.LocationManager
 import android.os.Bundle
 import android.os.IBinder
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
+import androidx.core.view.MenuProvider
 import androidx.core.view.isVisible
+import androidx.lifecycle.Lifecycle
 import com.dmitrybrant.altimeter.databinding.ActivityMainBinding
+import kotlin.math.abs
 
-class MainActivity : AppCompatActivity(), LocationListener, SensorEventListener {
+class MainActivity : AppCompatActivity(), MenuProvider, LocationListener, SensorEventListener {
     private lateinit var binding: ActivityMainBinding
 
     private var locationTracker: LocationTracker? = null
@@ -35,6 +42,9 @@ class MainActivity : AppCompatActivity(), LocationListener, SensorEventListener 
     private val magnetometerReading = FloatArray(3)
     private val rotationMatrix = FloatArray(9)
     private val orientationAngles = FloatArray(3)
+
+    private var latLongUnits = false
+    private var altUnits = false
 
     private val locationPermissionRequest = registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { permissions ->
         when {
@@ -50,8 +60,10 @@ class MainActivity : AppCompatActivity(), LocationListener, SensorEventListener 
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        restoreSettings()
 
         setSupportActionBar(binding.toolbar)
+        addMenuProvider(this, this, Lifecycle.State.RESUMED)
 
         sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
         geoMagField = GeomagneticField(0f, 0f, 0f, System.currentTimeMillis())
@@ -84,6 +96,32 @@ class MainActivity : AppCompatActivity(), LocationListener, SensorEventListener 
         locationTracker?.cleanup()
     }
 
+    override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+        menuInflater.inflate(R.menu.menu_main, menu)
+    }
+
+    override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+        return when (menuItem.itemId) {
+            R.id.menu_latlongunits -> {
+                latLongUnits = !latLongUnits
+                saveSettings()
+                updateLocationDisplay()
+                true
+            }
+            R.id.menu_altunits -> {
+                altUnits = !altUnits
+                saveSettings()
+                updateLocationDisplay()
+                true
+            }
+            R.id.menu_about -> {
+                //
+                true
+            }
+            else -> false
+        }
+    }
+
     private fun setUpLocationTracking() {
         if (haveLocationPermissions()) {
             startLocationTracking()
@@ -97,6 +135,23 @@ class MainActivity : AppCompatActivity(), LocationListener, SensorEventListener 
 
         binding.progressBar.isVisible = true
         binding.refreshLayout.isRefreshing = false
+    }
+
+    private fun saveSettings() {
+        val editor = getPrefs(this).edit()
+        editor.putBoolean(KEY_LATLONGUNITS, latLongUnits)
+        editor.putBoolean(KEY_ALTUNITS, altUnits)
+        editor.apply()
+    }
+
+    private fun restoreSettings() {
+        try {
+            val prefs = getPrefs(this)
+            latLongUnits = prefs.getBoolean(KEY_LATLONGUNITS, false)
+            altUnits = prefs.getBoolean(KEY_ALTUNITS, false)
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
     }
 
     private fun haveLocationPermissions(): Boolean {
@@ -116,14 +171,47 @@ class MainActivity : AppCompatActivity(), LocationListener, SensorEventListener 
     override fun onLocationChanged(location: Location) {
         lastLocation = location
         binding.progressBar.isVisible = false
-
-        binding.txtLatitude.text = location.latitude.toString()
-        binding.txtLongitude.text = location.longitude.toString()
-        binding.txtAltitude.text = (location.altitude * 3.28084).toInt().toString()
-
-        geoMagField = GeomagneticField(location.latitude.toFloat(), location.longitude.toFloat(),
-            location.altitude.toFloat(), System.currentTimeMillis())
+        updateLocationDisplay()
     }
+
+    private fun updateLocationDisplay() {
+        lastLocation?.let {
+            binding.txtLatitude.text = if (latLongUnits) decToHms(it.latitude) else it.latitude.toString()
+            binding.txtLongitude.text = if (latLongUnits) decToHms(it.longitude) else it.longitude.toString()
+            binding.txtAltitude.text = if (altUnits) ((it.altitude * 3.28084).toInt().toString() + " m") else (it.altitude.toInt().toString() + " ft")
+
+            geoMagField = GeomagneticField(it.latitude.toFloat(), it.longitude.toFloat(),
+                it.altitude.toFloat(), System.currentTimeMillis())
+        }
+    }
+
+    override fun onSensorChanged(event: SensorEvent) {
+        if (event.sensor.type == Sensor.TYPE_ACCELEROMETER) {
+            System.arraycopy(event.values, 0, accelerometerReading, 0, accelerometerReading.size)
+        } else if (event.sensor.type == Sensor.TYPE_MAGNETIC_FIELD) {
+            System.arraycopy(event.values, 0, magnetometerReading, 0, magnetometerReading.size)
+        }
+
+        updateCompass()
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+        //
+    }
+
+    private fun updateCompass() {
+        SensorManager.getRotationMatrix(rotationMatrix, null, accelerometerReading, magnetometerReading)
+
+        SensorManager.getOrientation(rotationMatrix, orientationAngles)
+        var degrees = -Math.toDegrees(orientationAngles[0].toDouble()).toFloat()
+
+        if (geoMagField != null) {
+            degrees -= geoMagField!!.declination
+        }
+
+        binding.imgCompass.rotation = degrees
+    }
+
 
     @SuppressLint("MissingPermission")
     class LocationTracker(
@@ -156,30 +244,21 @@ class MainActivity : AppCompatActivity(), LocationListener, SensorEventListener 
         }
     }
 
-    override fun onSensorChanged(event: SensorEvent) {
-        if (event.sensor.type == Sensor.TYPE_ACCELEROMETER) {
-            System.arraycopy(event.values, 0, accelerometerReading, 0, accelerometerReading.size)
-        } else if (event.sensor.type == Sensor.TYPE_MAGNETIC_FIELD) {
-            System.arraycopy(event.values, 0, magnetometerReading, 0, magnetometerReading.size)
+    companion object {
+        private const val PREFS_NAME = "AltimeterPrefs"
+        private const val KEY_LATLONGUNITS = "latLongUnits"
+        private const val KEY_ALTUNITS = "altUnits"
+
+        private fun getPrefs(context: Context): SharedPreferences {
+            return context.getSharedPreferences(PREFS_NAME, 0)
         }
 
-        updateCompass()
-    }
-
-    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
-        //
-    }
-
-    private fun updateCompass() {
-        SensorManager.getRotationMatrix(rotationMatrix, null, accelerometerReading, magnetometerReading)
-
-        SensorManager.getOrientation(rotationMatrix, orientationAngles)
-        var degrees = -Math.toDegrees(orientationAngles[0].toDouble()).toFloat()
-
-        if (geoMagField != null) {
-            degrees -= geoMagField!!.declination
+        private fun decToHms(dec: Double): String {
+            val d = abs(dec)
+            val deg = d.toInt()
+            val min = ((d - deg) * 60).toInt()
+            val sec = ((d - deg - min / 60.0) * 3600).toInt()
+            return String.format("%d° %d' %d\"", dec.toInt(), min, sec)
         }
-
-        binding.imgCompass.rotation = degrees
     }
 }
