d6e2bb1b316010b1601776566157e4bff240e535
Dmitry Brant <me@dmitrybrant.com>
2023-10-02 15:09:20 -0400

    Incorporate compass.

--- app/src/main/java/com/dmitrybrant/altimeter/MainActivity.kt
+++ app/src/main/java/com/dmitrybrant/altimeter/MainActivity.kt
@@ -3,24 +3,38 @@ package com.dmitrybrant.altimeter
 import android.Manifest
 import android.annotation.SuppressLint
 import android.app.Service
+import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.hardware.GeomagneticField
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import android.location.Location
 import android.location.LocationListener
 import android.location.LocationManager
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.IBinder
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.view.isVisible
 import com.dmitrybrant.altimeter.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity(), LocationListener {
+class MainActivity : AppCompatActivity(), LocationListener, SensorEventListener {
     private lateinit var binding: ActivityMainBinding
 
     private var locationTracker: LocationTracker? = null
+    private var lastLocation: Location? = null
+
+    private var geoMagField: GeomagneticField? = null
+    private lateinit var sensorManager: SensorManager
+    private val accelerometerReading = FloatArray(3)
+    private val magnetometerReading = FloatArray(3)
+    private val rotationMatrix = FloatArray(9)
+    private val orientationAngles = FloatArray(3)
 
     private val locationPermissionRequest = registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { permissions ->
         when {
@@ -37,6 +51,9 @@ class MainActivity : AppCompatActivity(), LocationListener {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        geoMagField = GeomagneticField(0f, 0f, 0f, System.currentTimeMillis())
+
         binding.progressBar.isVisible = true
 
         if (haveLocationPermissions()) {
@@ -44,6 +61,22 @@ class MainActivity : AppCompatActivity(), LocationListener {
         } else {
             locationPermissionRequest.launch(arrayOf(Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION))
         }
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)?.also { accelerometer ->
+            sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL, SensorManager.SENSOR_DELAY_UI)
+        }
+        sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD)?.also { magneticField ->
+            sensorManager.registerListener(this, magneticField, SensorManager.SENSOR_DELAY_NORMAL, SensorManager.SENSOR_DELAY_UI)
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        sensorManager.unregisterListener(this)
     }
 
     override fun onDestroy() {
@@ -66,12 +99,15 @@ class MainActivity : AppCompatActivity(), LocationListener {
     }
 
     override fun onLocationChanged(location: Location) {
+        lastLocation = location
         binding.progressBar.isVisible = false
 
         binding.txtLatitude.text = location.latitude.toString()
         binding.txtLongitude.text = location.longitude.toString()
         binding.txtAltitude.text = location.altitude.toInt().toString() //"Altitude: ${location.altitude.toInt()} m (${(location.altitude * 3.28084).toInt()} ft)"
 
+        geoMagField = GeomagneticField(location.latitude.toFloat(), location.longitude.toFloat(),
+            location.altitude.toFloat(), System.currentTimeMillis())
     }
 
     @SuppressLint("MissingPermission")
@@ -103,5 +139,32 @@ class MainActivity : AppCompatActivity(), LocationListener {
         override fun onBind(intent: Intent?): IBinder? {
             return null
         }
+    }
+
+    override fun onSensorChanged(event: SensorEvent) {
+        if (event.sensor.type == Sensor.TYPE_ACCELEROMETER) {
+            System.arraycopy(event.values, 0, accelerometerReading, 0, accelerometerReading.size)
+        } else if (event.sensor.type == Sensor.TYPE_MAGNETIC_FIELD) {
+            System.arraycopy(event.values, 0, magnetometerReading, 0, magnetometerReading.size)
+        }
+
+        updateCompass()
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+        //
+    }
+
+    private fun updateCompass() {
+        SensorManager.getRotationMatrix(rotationMatrix, null, accelerometerReading, magnetometerReading)
+
+        SensorManager.getOrientation(rotationMatrix, orientationAngles)
+        var degrees = -Math.toDegrees(orientationAngles[0].toDouble()).toFloat()
+
+        if (geoMagField != null) {
+            degrees -= geoMagField!!.declination
+        }
+
+        binding.imgCompass.rotation = degrees
     }
 }
